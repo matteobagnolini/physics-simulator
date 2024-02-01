330c7ce0fceb345c2c6bf34c63228a2ac6391f4e
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-02-01 18:18:05 +0100

    add loop for angles

--- src/RangeOfProjectile/RangeCalc.java
+++ src/RangeOfProjectile/RangeCalc.java
@@ -10,11 +10,6 @@ public class RangeCalc {
     private double angleInRads;
     private double v0;
 
-    RangeCalc(double speed, double angleInDegrees) {
-        this.v0 = speed;
-        this.angleInRads = Math.toRadians(angleInDegrees);
-    }
-
     /* Compute the maximum height of the projectile */
     public double height() {
         return (v0*Math.sin(angleInRads))*(v0*Math.sin(angleInRads)) / 2*G;
@@ -26,10 +21,24 @@ public class RangeCalc {
             Math.sin(angleInRads) * Math.cos(angleInRads) / G;
     }
 
+    public void setSpeed(final double speed) {
+        this.v0 = speed;
+    }
+
+    public void setAngle(final double angle) {
+        this.angleInRads = Math.toRadians(angle);
+    }
+
     public static void main(String[] args) {
-        RangeCalc rc = new RangeCalc(20, 30);
-        System.out.println("Range = " + rc.range() + " meters");
-        System.out.println("Max height: " + rc.height() + " meters");
+        RangeCalc rc = new RangeCalc();
+        rc.setSpeed(15);
+        for(double angle = 0; angle<= 90; angle += 5) {
+            rc.setAngle(angle);
+            System.out.println("Using angle = " + angle);
+            System.out.println("Range = " + rc.range() + " meters");
+            System.out.println("Max height: " + rc.height() + " meters");
+            System.out.println("##########################");
+        }
     }
 
 }
