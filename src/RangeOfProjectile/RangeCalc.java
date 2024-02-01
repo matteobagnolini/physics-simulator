42369f7d3ed92523dd877b5a4ab6c0a576524824
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-02-01 18:06:47 +0100

    add max height calculator

--- src/RangeOfProjectile/RangeCalc.java
+++ src/RangeOfProjectile/RangeCalc.java
@@ -1,24 +1,35 @@
 package RangeOfProjectile;
 
+
+/**
+ * 
+ */
 public class RangeCalc {
 
     private static double G = 9.81;
-    private double range;
+    private double angleInRads;
+    private double v0;
 
     RangeCalc(double speed, double angleInDegrees) {
-        double angleInRads;
-        angleInRads = Math.toRadians(angleInDegrees);
-        range = 2 * speed * speed * 
+        this.v0 = speed;
+        this.angleInRads = Math.toRadians(angleInDegrees);
+    }
+
+    /* Compute the maximum height of the projectile */
+    public double height() {
+        return (v0*Math.sin(angleInRads))*(v0*Math.sin(angleInRads)) / 2*G;
+    }
+
+    /* Compute the range of the projectile */
+    public double range() {
+        return 2 * v0 * v0 * 
             Math.sin(angleInRads) * Math.cos(angleInRads) / G;
     }
 
-    public double range() {
-        return this.range;
-    }
-
     public static void main(String[] args) {
-        RangeCalc rc = new RangeCalc(20, 90);
+        RangeCalc rc = new RangeCalc(20, 30);
         System.out.println("Range = " + rc.range() + " meters");
+        System.out.println("Max height: " + rc.height() + " meters");
     }
 
 }
